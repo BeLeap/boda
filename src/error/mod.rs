f29b767b9038b5eb48bad6513cba9670a450bd00
BeLeap <beleap@beleap.dev>
2025-04-28 02:32:51 +0900

    fix: remove unused dep
    fix: remove unused dep

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -3,7 +3,6 @@ use std::{fmt, io};
 pub type BodaResult<T> = Result<T, BodaError>;
 
 pub enum BodaError {
-    CtrlC(ctrlc::Error),
     Io(io::Error),
     Custom(String),
 }
@@ -11,7 +10,6 @@ pub enum BodaError {
 impl fmt::Display for BodaError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            BodaError::CtrlC(e) => write!(f, "Failed to setup ctrlc signal handler: {e}"),
             BodaError::Io(e) => write!(f, "Failed to handle io: {e}"),
             BodaError::Custom(e) => write!(f, "{}", e),
         }
@@ -27,12 +25,6 @@ impl fmt::Debug for BodaError {
 impl std::convert::From<io::Error> for BodaError {
     fn from(value: io::Error) -> Self {
         BodaError::Io(value)
-    }
-}
-
-impl std::convert::From<ctrlc::Error> for BodaError {
-    fn from(value: ctrlc::Error) -> Self {
-        BodaError::CtrlC(value)
     }
 }
 
