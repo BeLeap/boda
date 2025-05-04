20456d62319bbf5c397e5668e6cf46ce31287c75
BeLeap <beleap@beleap.dev>
2025-05-04 21:46:02 +0900

    chore: cleanup unused
    chore: cleanup unused

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,12 +1,1 @@
 pub mod manager;
-
-use std::process::Command;
-
-use crate::error;
-
-pub fn run(shell: String, command: Vec<String>) -> error::BodaResult<String> {
-    let command = command.join(" ");
-    let output = Command::new(shell).arg("-c").arg(command).output()?;
-
-    return Ok(String::from_utf8_lossy(&output.stdout).to_string());
-}
